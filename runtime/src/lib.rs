e54cc7ff42effbf4fd247c6a6fe70276cf3da3a8
JNhua <huajnan@foxmail.com>
2019-09-11 09:28:10 +0800

    change runtime_spec_name

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -111,8 +111,8 @@ pub mod opaque {
 
 /// This runtime version.
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-    spec_name: create_runtime_str!("contract-node"),
-    impl_name: create_runtime_str!("contract-node"),
+    spec_name: create_runtime_str!("turing-node"),
+    impl_name: create_runtime_str!("turing-node"),
     authoring_version: 3,
     spec_version: 4,
     impl_version: 4,
